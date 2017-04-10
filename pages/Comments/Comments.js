19b09c5c7d240b656ab22d76e1962c2b4ad3ca13
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-04-10 16:50:26 +0530

    Adds styling for comments page

--- pages/Comments/Comments.js
+++ pages/Comments/Comments.js
@@ -1,19 +1,45 @@
 import { h, Component } from 'preact';
 import { connect } from 'preact-redux';
 
+import PostSummary from 'components/PostSummary/PostSummary';
 import * as actions from 'actions';
 
+import * as styles from './CommentsStyles.js';
+import * as postStyles from 'components/PostSummary/PostSummaryStyles.js';
+
 class Comment extends Component {
-    render() {
-        return <li>
-                {this.props.data.body}
-                <ul>
-                    {this.props.data.replies
-                        ? this.props.data.replies.data.children.map(comment => comment.kind === 't1' ? <Comment data={comment.data} /> : null)
+    constructor(props) {
+        super(props);
+        this.state = {
+            showReplies: false
+        }
+    }
+    toggleReplies() {
+        console.log('toggling', this.state);
+        this.setState({
+            showReplies: !this.state.showReplies
+        });
+    }
+    render({data}) {
+        return <div {...styles.comment}>
+                <div {...styles.user}>{`/u/${this.props.data.author}`}</div>
+                <div {...styles.text}>
+                    {data.body}
+                    {this.state.showReplies
+                        ? <div>
+                            {data.replies
+                                ? data.replies.data.children.map(comment => comment.kind === 't1' ? <Comment data={comment.data} /> : null)
+                                : null
+                            }
+                        </div>
                         : null
                     }
-                </ul>
-            </li>
+                </div>
+                <div {...styles.postMeta}>
+                    <span {...postStyles.upvoteIcon}>&#10148;</span> {Number(data.ups).toLocaleString()}
+                    {data.replies.data ? <span {...styles.repliesCount} onClick={this.toggleReplies.bind(this)}>{Number(data.replies.data.children.length).toLocaleString()} Replies</span> : null}
+                </div>
+            </div>
     }
 }
 
@@ -29,14 +55,11 @@ class Comments extends Component {
     }
     render() {
         return !this.props.isFetching
-            ? <div>
-                {this.props.comments[0].data.children[0].data.ups} | {this.props.comments[0].data.children[0].data.title}
-                <hr/>
-                <ul>
-                    {this.props.comments[1].data.children.map(comment => <Comment data={comment.data} />)}
-                </ul>
+            ? <div {...styles.page}>
+                <PostSummary data={this.props.comments[0].data.children[0].data} />
+                {this.props.comments[1].data.children.map(comment => comment.kind === 't1' ? <Comment data={comment.data} /> : null)}
             </div>
-            : 'loading'
+            : 'Loading'
     }
 }
 
---
+++ pages/Comments/CommentsStyles.js
@@ -0,0 +1,30 @@
+import { css } from 'glamor';
+
+export const page = css({
+    backgroundColor: '#f6f6f6'
+});
+
+export const comment = css({
+    margin: '5px',
+    padding: '10px 10px 0 10px',
+    border: '1px solid #ddd',
+    backgroundColor: '#fff'
+});
+
+export const user = css({
+    color: '#666'
+});
+
+export const text = css({
+    marginTop: '5px'
+});
+
+export const postMeta = css({
+    color: '#666',
+    fontSize: 'smaller'
+});
+
+export const repliesCount = css({
+    padding: '10px',
+    display: 'inline-block'
+});
