1d715450714603424d25f9df9b53698938c58068
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-04-12 13:10:30 +0530

    Changes for running app on heroku (#1)
    * Server on port 80
    
    * Reading port from env
    
    * Moves express and spdy to deps
    
    * Moves bundles to static and adds to git
    
    * Adds log to server

--- server.js
+++ server.js
@@ -6,18 +6,19 @@ const fs = require('fs');
 const app = express();
 const port = 8080;
 
-app.use(express.static('build'));
+app.use(express.static('static'));
 
 app.get('*', (req, res) => {
   res.type('html').sendFile(path.join(__dirname, 'index.html'));
 });
 
-const credentials = {
-    key: fs.readFileSync(path.join(__dirname, 'certificates', 'my.flipkart.com.key')),
-    cert:  fs.readFileSync(path.join(__dirname, 'certificates', 'my.flipkart.com.crt'))
-};
+// const credentials = {
+//     key: fs.readFileSync(path.join(__dirname, 'certificates', 'my.flipkart.com.key')),
+//     cert:  fs.readFileSync(path.join(__dirname, 'certificates', 'my.flipkart.com.crt'))
+// };
 
-app.listen(port);
+app.listen(process.env.PORT || port);
+console.log('Server listening on port', process.env.PORT || port);
 
 // spdy
 //   .createServer(credentials, app)
