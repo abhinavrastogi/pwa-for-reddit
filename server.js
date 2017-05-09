01817cf54a84acda33d7948cfe2a979fbc3d0970
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-05-09 23:54:25 +0530

    Reauth impl (#3)
    * reject missing tokens
    
    * log json
    
    * https
    
    * show error
    
    * log cookies
    
    * more logging
    
    * disable logging
    
    * cookie maxAge

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.get('/auth', (req, res) => {
         headers: {
             'Authorization': `Basic ${base64encodedData}`
         },
-        body: `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http%3A%2F%2Fpwa-for-reddit.herokuapp.com%2Fauth`
+        body: `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https%3A%2F%2Fpwa-for-reddit.herokuapp.com%2Fauth`
     })
     .then(response => response.json())
     .then(json => {
         if(json.error) return Promise.reject(json.error);
-        if(json.access_token && json.refresh_token) res.cookie('access_token', json.access_token, {secure: true, maxAge: json.expires_in}).cookie('refresh_token', json.refresh_token, {secure: true, maxAge: 604800}).redirect(`/`);
+        if(json.access_token && json.refresh_token) res.cookie('access_token', json.access_token, {secure: true, maxAge: json.expires_in * 1000}).cookie('refresh_token', json.refresh_token, {secure: true, maxAge: 604800 * 1000}).redirect(`/`);
     })
     .catch(err => { res.status(401).send('Auth failure!'); })
 });
@@ -32,19 +32,20 @@ app.get('*', (req, res) => {
     const ignoreAuthOnPaths = ['.map', '.ico'];
 
     if(ignoreAuthOnPaths.indexOf(req.path.substr(-4)) < 0 && req.cookies.refresh_token && !req.cookies.access_token) {
-        console.log('revalidating');
         fetch('https://www.reddit.com/api/v1/access_token', {
             method: 'POST',
             headers: {
                 'Authorization': `Basic ${base64encodedData}`
             },
-            body: `grant_type=refresh_token&refresh_token=${req.cookies.refresh_token}&redirect_uri=http%3A%2F%2Fpwa-for-reddit.herokuapp.com%2Fauth`
+            body: `grant_type=refresh_token&refresh_token=${req.cookies.refresh_token}&redirect_uri=https%3A%2F%2Fpwa-for-reddit.herokuapp.com%2Fauth`
         })
         .then(response => response.json())
         .then(json => {
             if(json.error) return Promise.reject(json.error);
-            if(json.access_token && json.refresh_token) {
-                res.cookie('access_token', json.access_token, {secure: true, maxAge: json.expires_in}).type('html').set({'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload'}).sendFile(path.join(__dirname, 'index.html'));
+            if(json.access_token) {
+                res.cookie('access_token', json.access_token, {secure: true, maxAge: json.expires_in * 1000}).type('html').set({'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload'}).sendFile(path.join(__dirname, 'index.html'));
+            } else {
+                return Promise.reject('No token received');
             }
         })
         .catch(err => { res.status(401).send('Auth failure!'); })
