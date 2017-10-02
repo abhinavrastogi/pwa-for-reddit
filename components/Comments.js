8d067c3d804f781f6ba2f69acfa102b56cb1e2b2
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-10-02 13:33:38 +0530

    markdown comments

--- components/Comments.js
+++ components/Comments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import glam from 'glamorous';
+import ReactMarkdown from 'react-markdown';
 
 import Post from './Post';
 import {formatTimeAgo} from './utils';
@@ -32,10 +33,10 @@ export default class Comments extends Component {
 
 const Comment = ({data}) => <li>
 	<CommentBody>
-		<Meta>u/{data.author} &bull; {formatTimeAgo(data.created_utc)} &bull; {Number(data.score).toLocaleString()} votes</Meta>
-		{data.body}
+		<Meta>u/{data.author} &bull; {formatTimeAgo(data.created_utc)} &bull; {Number(data.score).toLocaleString()} votes &bull; {data.replies ? data.replies.data.children.length : '0'} replies</Meta>
+		{data.body ? <ReactMarkdown source={data.body} /> : null}
 	</CommentBody>
-	{data.replies && data.replies.data.children.length 
+	{data.replies && data.replies.data.children.length && !data.collapsed
 	? <Replies>
 		{data.replies.data.children.filter(child => child.kind == 't1').map(({data}) => <Comment key={data.id} data={data} />)}	
 	</Replies> 
@@ -56,7 +57,16 @@ const Replies = glam.ul({
 })
 
 const CommentBody = glam.div({
-	padding: '0 10px 10px 10px'
+	padding: '0 10px 10px 10px',
+	'& a': {
+		color: 'red'
+	},
+	'& p:nth-child(1)': {
+		marginTop: 0
+	},
+	'& p:nth-last-child(1)': {
+		marginBottom: 0
+	}
 })
 
 const Meta = glam.div({
